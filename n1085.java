fd582a4543aa2b3dc5c0a397fe6a2dc120ef6ee7
omj9803 <myeongju0316@gmail.com>
2021-10-10 20:07:48 +0900

    [1085] 직사각형에서 탈출

---
+++ n1085.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class n1085 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        int w = scanner.nextInt();
+        int h = scanner.nextInt();
+        int min = 0;
+        if (h - y > w - x) {
+            min = w - x;
+        } else {
+            min = h - y;
+        }
+        if (x > y) {
+            if (min > y) min = y;
+        } else {
+            if (min > x) min = x;
+        }
+        System.out.println(min);
+    }
+}
