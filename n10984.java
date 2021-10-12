c0aad58b385a6678a79d03d69cc474c26de90ce2
omj9803 <myeongju0316@gmail.com>
2021-10-13 05:53:18 +0900

    [10984] 내 학점을 구해줘

---
+++ n10984.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class n10984 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        for (int i = 0; i < num; i++) {
+            int subjects = scanner.nextInt();
+            int sum = 0;
+            double avg = 0;
+            for (int j = 0; j < subjects; j++) {
+                int sub = scanner.nextInt();
+                double score = scanner.nextDouble();
+                sum = sum + sub;
+                avg = avg + (sub * score);
+            }
+            System.out.print(sum + " ");
+            System.out.print(String.format("%.1f", avg/sum));
+            System.out.println();
+        }
+    }
+}
