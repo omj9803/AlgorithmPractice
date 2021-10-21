9c3e00573d0827331ab81ac5235ec92eeb08d0d3
omj9803 <myeongju0316@gmail.com>
2021-10-21 22:11:14 +0900

    [2752] 세수정렬

---
+++ n2752.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class n2752 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = new int[3];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] > numbers[j]) {
+                    int temp = numbers[i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+    }
+}
