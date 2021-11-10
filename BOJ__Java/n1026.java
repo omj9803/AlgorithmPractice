05794ee26970daad5d33cf0bd5208900d1bcdfd6
Oh Myeong Ju <34434135+omj9803@users.noreply.github.com>
2021-11-10 18:22:02 +0900

    [1026] 보물

--- BOJ__Java/n1026.java
+++ BOJ__Java/n1026.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class n1026 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int num = scanner.nextInt();
+        int num = scanner.nextInt(); 
         int[] a = new int[num];
         int[] b = new int[num];
         for (int i = 0; i < num; i++) {
