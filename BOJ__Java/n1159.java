9d4b26818b395bf2aa4337d809e5f319402b33c2
Oh Myeong Ju <34434135+omj9803@users.noreply.github.com>
2021-11-11 17:40:50 +0900

    [1159] 농구 경기

--- BOJ__Java/n1159.java
+++ BOJ__Java/n1159.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class n1159 {
+public class n1159 { 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int num = scanner.nextInt();
