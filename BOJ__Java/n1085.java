b925580e6ab010063408e4bc7f50b07ad7282203
Oh Myeong Ju <34434135+omj9803@users.noreply.github.com>
2021-11-11 17:40:07 +0900

    [1085] 직사각형에서 탈출

--- BOJ__Java/n1085.java
+++ BOJ__Java/n1085.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class n1085 {
+public class n1085 { 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int x = scanner.nextInt();
