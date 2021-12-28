4c712c4a133d9111f0875c9108ae08546817faca
omj9803 <myeongju0316@gmail.com>
2021-12-28 15:03:28 +0900

    [2789] 유학 금지

---
+++ BOJ__Java/n2789.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class n2789 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+//        char[] inputArray = input.toCharArray();
+        String cipher = "CAMBRIDGE";
+//        char[] cipherArray = cipher.toCharArray();
+        if (input.contains("C")){
+            
+        }
+    }
+}
