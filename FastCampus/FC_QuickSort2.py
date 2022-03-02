dd469ef49324182f7dcea473ffd15f1c339a8544
omj9803 <myeongju0316@gmail.com>
2022-03-02 19:08:05 +0900

    [FC]QuickSort 실습 - 수정

--- FastCampus/FC_QuickSort2.py
+++ FastCampus/FC_QuickSort2.py
@@ -1,13 +1,17 @@
 def qsort(data):
     if len(data) <= 1:
         return data
+
     pivot = data[0]
 
-    left = [item for item in data[1:] if data[item] < pivot]
-    right = [item for item in data[1:] if data[item] >= pivot]
+    left = [item for item in data[1:] if item > pivot]
+    right = [item for item in data[1:] if item <= pivot]
 
     return qsort(left) + [pivot] + qsort(right)
 
 # 시간 복잡도
 # 평균 - O(n log n)
 # 최악 - O(n^2) pivot이 가장 작거나 큰 경우
+
+data_list = [3,0,6,1,5]
+print(qsort(data_list))
