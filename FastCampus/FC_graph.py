78f2ac772da65684e999238e72d2b0e08c3f3729
omj9803 <myeongju0316@gmail.com>
2022-05-09 18:25:13 +0900

    graph example

---
+++ FastCampus/FC_graph.py
@@ -0,0 +1,13 @@
+graph = dict()
+
+# 그래프 표현 - dictionary, list
+graph['A'] = ['B', 'C']
+graph['B'] = ['A', 'D']
+graph['C'] = ['A', 'G', 'H', 'I']
+graph['D'] = ['B', 'E', 'F']
+graph['E'] = ['D']
+graph['F'] = ['D']
+graph['G'] = ['C']
+graph['H'] = ['C']
+graph['I'] = ['C', 'J']
+graph['J'] = ['I']
