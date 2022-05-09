4070566f97ebc4267e6b6afff97710dce5e731b9
omj9803 <myeongju0316@gmail.com>
2022-05-09 18:24:44 +0900

    bfs practice

---
+++ FastCampus/FC_bfs.py
@@ -0,0 +1,30 @@
+# bfs : 너비 우선 탐색
+# need_visit : 방문할 필요가 있는 노드들의 큐
+# visited : 방문했던 노드들의 큐
+
+graph = dict()
+
+graph['A'] = ['B', 'C']
+graph['B'] = ['A', 'D']
+graph['C'] = ['A', 'G', 'H', 'I']
+graph['D'] = ['B', 'E', 'F']
+graph['E'] = ['D']
+graph['F'] = ['D']
+graph['G'] = ['C']
+graph['H'] = ['C']
+graph['I'] = ['C', 'J']
+graph['J'] = ['I']
+
+def bfs(graph, start_node):
+    visited = list()
+    need_visit = list()
+
+    need_visit.append(start_node)
+    while need_visit:
+        node = need_visit.pop(0)
+        if node not in visited:
+            visited.append(node)
+            need_visit.extend(graph[node])
+    return visited
+
+print(bfs(graph, 'A'))
